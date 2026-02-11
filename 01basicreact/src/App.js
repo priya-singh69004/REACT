d78685e74a9715d28bd711f8cb4a14307e3dba01
priya-singh69004 <priyasingh69004@gmail.com>
2026-02-11 14:03:21 +0530

    basicreact

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+import Learn from './Learn'
+function App() {
+  return (
+    <>
+    <Learn/>
+    <h1> Learning React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Learn.js
@@ -0,0 +1,7 @@
+function Learn(){
+  return(
+    <h2>Learn React</h2>
+  )
+}
+
+export default Learn
