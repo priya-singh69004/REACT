9508a92ffd44bd1e49a0a4dae1bff6020576e309
priya-singh69004 <priyasingh69004@gmail.com>
2026-02-11 14:07:58 +0530

    counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter , setCounter] = useState(5)
+
+  // let counter = 5
+
+  const addValue = () => {
+    if(counter < 20){
+      console.log("clicked" , Math.random());
+      // counter = counter + 1
+      // setCounter(counter + 1 )
+      setCounter(prevCounter => prevCounter + 1 )
+    }
+    
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      console.log("clicked" , Math.random());
+      counter = counter -1;
+      setCounter(counter)
+    }
+   
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button
+      onClick = {addValue}
+      >Add Value</button>
+      <br />
+      <button
+      onClick = {removeValue}
+      >Remove Value</button>
+    </>
+  )
+}
+
+export default App
